5cef0e8d973f24f9e7f396bd709f3746ddc77010
Kosikowski <8352262+Kosikowski@users.noreply.github.com>
2025-07-05 14:43:08 +0100

    Initial Commit

---
+++ RubicCube/RubicCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RubicCubeApp.swift
+//  RubicCube
+//
+//  Created by Mateusz Kosikowski on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct RubicCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RubicCubeTests/RubicCubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RubicCubeTests.swift
+//  RubicCubeTests
+//
+//  Created by Mateusz Kosikowski on 05/07/2025.
+//
+
+import Testing
+@testable import RubicCube
+
+struct RubicCubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
